17317caaf3631abcec7650022f61128705edb464
vinayak <kkvinayak718.com>
2026-01-12 15:10:21 +0530

    update

--- school_backend/settings.py
+++ school_backend/settings.py
@@ -25,14 +25,6 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
-INSTALLED_APPS += ["corsheaders"]
-
-MIDDLEWARE = ["corsheaders.middleware.CorsMiddleware"] + MIDDLEWARE
-
-CORS_ALLOWED_ORIGINS = [
-    "https://ths-frontend-p8v4.onrender.com",
-]
-
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -75,13 +67,19 @@ MIDDLEWARE = [
 # -------------------------------------------------
 # CORS / CSRF
 # -------------------------------------------------
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ths-frontend-p8v4.onrender.com",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://127.0.0.1",
     "http://localhost:5173",
+    "https://ths-frontend-p8v4.onrender.com",
 ]
 
 # -------------------------------------------------
